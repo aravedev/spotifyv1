dc9a1c716d11822177373bb7ebb3be4d4ff253eb
aravedev <dravedev@gmail.com>
2022-10-03 21:55:45 +0200

    Added layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Sidebar = () => {
+  return <Container>Sidebar</Container>;
+};
+
+const Container = styled.div`
+  background-color: black;
+`;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from "./Body";
+export * from "./Footer";
+export * from "./Login";
+export * from "./Navbar";
+export * from "./Sidebar";
+export * from "./Spotify";
